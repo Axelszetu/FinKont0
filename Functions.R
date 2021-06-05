f45607b7e47e1d09a36f2111978491d78eb32f4b
Axelszetu <76113789+Axelszetu@users.noreply.github.com>
2021-06-05 16:46:54 +0200

    Code for working with empirical data.

--- Functions.R
+++ Functions.R
@@ -90,3 +90,46 @@ MC_convergence_tracker <- function(mu = 0.07, sigma = 0.2, dt = 1/255, M = 1, n 
   df <- data.frame(x = x, means, dt = factor)
   df
 }
+
+option_pricer_analytical <- function(S = 1, K = 1, time = 1, r = 0.03, sigma = 0.2){
+  d1 <- 1/(sigma*sqrt(time)) * (log(S/K) + (r + ((sigma^2)/2)*(time)))
+  d2 <- d1 - sigma*sqrt(time)
+  price <- S*pnorm(d1) - exp(-r*time)*K*pnorm(d2)
+  price
+}
+
+option_pricer_MC <- function(n = 10000, S = 1, K = 1, time = 1, r = 0.03, sigma = 0.2, withplot = FALSE){
+  discount_factor <- exp(-r*time)
+  prices <- rlnorm(n = n, meanlog = (r - (sigma^2)/2)*time, sdlog = sigma)
+  payoffs <- prices - K
+  inthemoney <- (payoffs >= 0)
+  payoffs <- payoffs*inthemoney
+  payoffs <- payoffs*discount_factor
+  estimates <- c(mean(payoffs), sd(payoffs)/sqrt(n))
+  out <- list()
+  out$estimates <- estimates
+  if (withplot == TRUE){
+    d1 <- 1/(sigma*sqrt(time)) * (log(S/K) + (r + ((sigma^2)/2)*(time)))
+    d2 <- d1 - sigma*sqrt(time)
+    BS_price <- S*pnorm(d1) - exp(-r*time)*K*pnorm(d2)
+    
+    estimate_n <- numeric(length = n)
+    var_n <- numeric(length = n)
+    sd_n <- numeric(length = n)
+    for (i in (1:n)){
+      estimate_n[i] <- mean(payoffs[1:i])
+      var_n[i] <- mean((payoffs[1:i] - estimate_n[i])^2)/n
+      sd_n[i] <- sqrt(var_n[i])
+    }
+    
+    running_estimates <- data.frame(estimate_n, sd_n)
+    out$plot <- ggplot(running_estimates, aes(x = c(1:n), y = estimate_n)) +
+      geom_ribbon(mapping = aes(
+        ymin = estimate_n - 1.96*sd_n,
+        ymax = estimate_n + 1.96*sd_n
+      ), fill = "gray"
+      ) + geom_point() + geom_line() +
+      geom_hline(yintercept = BS_price, color = "red")
+  }
+  out
+}
--- Option data.R
+++ Option data.R
@@ -7,3 +7,44 @@ library(tidyverse)
 #                                    data_source = "yahoo")
 
 load("euro_option_data.RData")
+
+options_aug31 <- euro_option_data[["Sep.30.2021"]][["calls"]]
+traded <- options_aug31[["IV"]] > 0.01
+traded_options_aug31 <- options_aug31[traded, ]
+
+#Data was retrieced on 04/06/2021
+#Adjusted close price was 4229.89
+
+S <- 4229.89
+
+traded_options_aug31$logmoneyness <- log(S/traded_options_aug31$Strike)
+volatility_smile <- ggplot(traded_options_aug31, aes(x = logmoneyness, y = IV)) + geom_point()
+
+traded_options_aug31$model_price <- option_pricer_analytical(S = S, K = traded_options_aug31$Strike, time = 0.25, r = 0.0002, sigma = 0.164302)
+
+plot_data_emipirical <- data.frame(Strike = traded_options_aug31$Strike, Price = traded_options_aug31$Last, Label = "Empirical")
+plot_data_theoretical <- data.frame(Strike = traded_options_aug31$Strike, Price =  traded_options_aug31$model_price, Label = "Theoretical")
+graph_data <- rbind(plot_data_emipirical, plot_data_theoretical)
+prices_plot <- ggplot(graph_data, aes(x = Strike, y = Price, color = Label)) + geom_point() + geom_vline(aes(xintercept = 4229.89))
+prices_plot
+
+traded_options_aug31[1,]
+
+goal_function_maker <- function(K, L){
+  goal_function <- function(sigma){
+    d1 <- 1/(sigma*sqrt(0.25)) * (log(4229.89/K) + (0.0002 + ((sigma^2)/2)*(0.25)))
+    d2 <- d1 - sigma*sqrt(0.25)
+    price <- 4229.89*pnorm(d1) - exp(-0.0002*0.25)*K*pnorm(d2)
+    out <- abs(price - L)
+    out
+  }
+  goal_function
+}
+
+test_function <- goal_function_maker(K = 4220, L = 116.80)
+optimize(test_function, interval = c(0,1), maximum = F)
+
+my_IV <- numeric(length = 209)
+for (i in (1:209)){
+  my_IV[i] <- optimize(goal_function_maker(traded_options_aug31$Strike[i], traded_options_aug31$Ask[i]), interval = c(0,1), maximum = F)$minimum
+}
